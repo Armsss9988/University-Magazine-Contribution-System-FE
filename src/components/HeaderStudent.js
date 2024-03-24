64a54c9dc509c354ef613849de78b50a70c2072e
Minh Trí Trần <tritranrpc@gmail.com>
2024-03-25 00:02:15 +0700

    fix header student

--- src/components/HeaderStudent.js
+++ src/components/HeaderStudent.js
@@ -1,49 +1,41 @@
 import React, { useState, useEffect } from "react";
 import "../screens/css/styles.css";
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
 import LogoutButton from "./Logout";
 
-
 function HeaderStudent() {
-
-
     const [activeTab, setActiveTab] = useState('/');
 
     const handleClick = (path) => {
         setActiveTab(path);
     };
 
+    // Cập nhật state activeTab khi đường dẫn thay đổi
+    useEffect(() => {
+        setActiveTab(window.location.pathname);
+    }, []);
 
     return (
         <header>
             <div className="logoname">
                 <h2 style={{ fontSize: "20px" }}>FGW University Magazine Contribution</h2>
-
             </div>
 
-            <div className="menu">
-            <p >
-                    <Link style={{
-                        fontSize: '20px', fontWeight: "normal", textDecoration:'none', color: 'black'
-                    }} to="/studenthome">Home</Link>
-                </p>
-                <p >
-                    <Link style={{
-                        fontSize: '20px', fontWeight: "normal", marginRight: '100px',
-                        marginLeft: '100px', color: 'black', textDecoration:'none'
-                    }} to="/listsubmission">My submission</Link>
+            <div className="menuStudent">
+                <p>
+                    <Link className={activeTab === '/studenthome' ? 'active' : ''} to="/studenthome" onClick={() => handleClick('/studenthome')}>Home</Link>
                 </p>
                 
-                
-                
+                <p>
+                    <Link className={activeTab === '/listsubmission' ? 'active' : ''} to="/listsubmission" onClick={() => handleClick('/listsubmission')}>My submission</Link>
+                </p>
             </div>
 
             <div>
                 <LogoutButton />
             </div>
-
         </header>
     );
 }
 
-export default HeaderStudent
+export default HeaderStudent;
